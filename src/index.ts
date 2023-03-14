3b4abef223e418d34c107f5a5aa4a1b220f8efa0
magnushaling <122467690+magnushaling@users.noreply.github.com>
2023-03-14 17:45:43 +0200

    asd

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import bodyParser from "body-parser";
 import express from "express";
+import cors from "cors";
 import pg from "pg";
 
 // Connect to the database using the DATABASE_URL environment
@@ -9,13 +9,12 @@ const pool = new pg.Pool();
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(cors());
+app.use(express.json());
 
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+app.get("/treks", async (req, res) => {
+  const { rows } = await pool.query("SELECT * FROM treks;");
+  res.send(JSON.stringify(rows));
 });
 
 app.listen(port, () => {
